ebc09e52c546af1c08333e427637dc753c13dd02
Jason Rauen <jason.rauen@gmail.com>
2020-11-07 12:08:01 -0500

    major update? idk, reinstalling system so just throwing this up into the cloud.

--- dist.go
+++ dist.go
@@ -9,14 +9,22 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
 	"time"
 
 	"github.com/vugu/vugu/distutil"
 )
 
 func main() {
+	// chmod splunk source
+	cmd := exec.Command("/bin/sh", "-c", "sudo chown -R jrauen: dist/password_manager")
+	cmd.Output()
+	cmd = exec.Command("/bin/sh", "-c", "sudo chmod -R 777 src/splunk/*")
+	cmd.Output()
+	cmd = exec.Command("/bin/sh", "-c", "sudo chmod -R 777 dist/password_manager")
+	cmd.Output()
 
-	clean := flag.Bool("clean", true, "Remove dist dir before starting")
+	clean := flag.Bool("clean", false, "Remove dist dir before starting")
 	dist := flag.String("dist", "dist", "Directory to put distribution files in")
 	flag.Parse()
 
@@ -28,11 +36,11 @@ func main() {
 
 	os.MkdirAll(*dist, 0755) // create dist dir if not there
 
-	// copy static files
-	distutil.MustCopyDirFiltered(".", *dist, nil)
+	// copy splunk app
+	distutil.MustCopyDirFiltered("src/splunk", *dist, regexp.MustCompile(`[.]*`))
 
 	// find and copy wasm_exec.js
-	distutil.MustCopyFile(distutil.MustWasmExecJsPath(), filepath.Join(*dist, "password_manager.js"))
+	// distutil.MustCopyFile(distutil.MustWasmExecJsPath(), filepath.Join(*dist, "password_manager.js"))
 
 	// check for vugugen and go get if not there
 	if _, err := exec.LookPath("vugugen"); err != nil {
@@ -43,7 +51,7 @@ func main() {
 	fmt.Print(distutil.MustExec("go", "generate", "."))
 
 	// run go build for wasm binary
-	fmt.Print(distutil.MustEnvExec([]string{"GOOS=js", "GOARCH=wasm"}, "go", "build", "-o", filepath.Join(*dist, "password_manager.wasm"), "."))
+	fmt.Print(distutil.MustEnvExec([]string{"GOOS=js", "GOARCH=wasm"}, "go", "build", "-o", filepath.Join(*dist, "password_manager/appserver/static/password_manager.wasm"), "./src/go"))
 
 	// STATIC INDEX FILE:
 	// if you are hosting with a static file server or CDN, you can write out the default index.html from simplehttp
